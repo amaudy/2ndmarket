ebaedbe0a0ff3fd7779e16e9c1efd0b6401ebed0
dev <dev@local.local>
2024-12-21 21:50:29 +0700

    Update homepage

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,4 +1,10 @@
 from django.views.generic import TemplateView
+from apps.categories.models import MainCategory
 
 class HomeView(TemplateView):
-    template_name = "core/home.html" 
+    template_name = "core/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = MainCategory.objects.filter(is_active=True)[:6]
+        return context 

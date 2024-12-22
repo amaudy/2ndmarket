748d95718b39e084ba66406d8fb1aaa1d6e5ac46
dev <dev@local.local>
2024-12-22 11:40:35 +0700

    Fixed bug when visit listing detail

--- apps/listings/urls.py
+++ apps/listings/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path('<int:pk>/', views.ListingDetailView.as_view(), name='detail'),
     path('<int:pk>/edit/', views.EditListingView.as_view(), name='edit'),
     path('images/<int:pk>/delete/', views.delete_listing_image, name='delete_image'),
+    path('<int:pk>/comments/add/', views.AddCommentView.as_view(), name='add_comment'),
+    path('comments/<int:pk>/edit/', views.EditCommentView.as_view(), name='edit_comment'),
+    path('comments/<int:pk>/delete/', views.DeleteCommentView.as_view(), name='delete_comment'),
     path('<int:pk>/buy/', views.CreateOrderView.as_view(), name='buy'),
     path('orders/<int:pk>/confirm/', views.OrderConfirmView.as_view(), name='order_confirm'),
 ] 
